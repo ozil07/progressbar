f430c46e12960b0a12ec6cb0e3bdc672aad307b6
Сайпула <ozil7@mail.ru>
2017-10-18 12:54:09 +0300

    Прогресс бар

---
+++ main.js
@@ -0,0 +1,16 @@
+﻿"use strict";
+
+var bodyHeight = document.body.scrollHeight - document.documentElement.clientHeight,
+		progressBar = document.querySelector('.progress__bar');
+
+
+document.body.onscroll = function(e) {
+	var scrollArea = document.documentElement.scrollTop;
+	var sizeBg = scrollArea * 100 / bodyHeight;
+	progressBar.style.backgroundSize = sizeBg + '%' + ' 100%';
+}
+
+
+
+
+		
